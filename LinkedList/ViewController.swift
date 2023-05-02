210ef91c63944c47e93e19c80a552bb629199ec0
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2023-05-02 12:25:42 -0300

    Initial Commit

---
+++ LinkedList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkedList
+//
+//  Created by Vanesa Korbenfeld on 02/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
